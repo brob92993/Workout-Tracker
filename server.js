bee099ba5535f429e62d4d775d12c4f7967268e3
Brian Robinson <brob92993@gmail.com>
2021-08-20 09:37:11 -0400

    deploying

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(express.static('public'));
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/Fitness";
 mongoose.connect(MONGODB_URI,{  
     useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
     useFindAndModify:false
-})
+    }
+);
 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
